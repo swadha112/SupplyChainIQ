2e1ea1b69032ddbbb84771c8154923c91634a413
swadha112 <swadha.khatod07@gmail.com>
2024-10-03 08:00:10 +0530

    css in Breadcrumb

--- frontend/src/components/Breadcrumbs/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumbs/Breadcrumb.tsx
@@ -16,7 +16,7 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
               Dashboard /
             </Link>
           </li>
-          <li className="font-medium text-primary">{pageName}</li>
+          <li className="font-medium text-primary dark:text-green">{pageName}</li>
         </ol>
       </nav>
     </div>
